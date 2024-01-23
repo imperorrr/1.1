6b04422aad4aa6e48529afa2dc7e4beb2a915a4a
Pёtr <petryimba@gmail.com>
2024-01-23 23:31:31 +0300

    1.1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 69_000;
+        int bonus = price / 20;
+        System.out.println("Бонус: " + bonus);
+    }
+}
